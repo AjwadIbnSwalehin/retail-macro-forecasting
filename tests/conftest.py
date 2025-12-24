43851329f33be3ecc636b6d11e3e03423590a00b
Ajwad <aibnswalehin@gmail.com>
2025-12-24 23:41:34 +0000

    Change root path when testing

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# Add project root to Python path
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
