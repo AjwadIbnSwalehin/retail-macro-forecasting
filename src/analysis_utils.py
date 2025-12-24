d6dd1e14a54055c48dacb901b5da5bc9f8e4b68c
Ajwad <aibnswalehin@gmail.com>
2025-12-24 23:42:58 +0000

    Moved math functions to python file for testing

--- src/analysis_utils.py
+++ src/analysis_utils.py
@@ -1,5 +1,6 @@
-import  pandas as pd
+from sklearn.metrics import mean_squared_error
 import numpy as np
+
 
 def compute_yearly_averages(df):
     yearly_averages = []
@@ -8,3 +9,20 @@ def compute_yearly_averages(df):
         yearly_averages.append(average)
     return yearly_averages
 
+
+def fit_polynomial_models(X_train, y_train, X_test, y_test, orders=[1, 2, 3]):
+    polynomials = {}
+    mse_values = {}
+
+    # Fit models
+    for order in orders:
+        coeffs = np.polyfit(X_train, y_train, order)
+        model = np.poly1d(coeffs)
+
+        y_pred = model(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+
+        polynomials[order] = model
+        mse_values[order] = mse
+    
+    return polynomials, mse_values
