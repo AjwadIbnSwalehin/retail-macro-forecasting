1087b3a98b1ceda99e6c532d5a207a3ca0658e0a
Ajwad <aibnswalehin@gmail.com>
2025-12-30 12:15:30 +0000

    Made calculating training data and forecasting modular

--- src/analysis_utils.py
+++ src/analysis_utils.py
@@ -38,3 +38,32 @@ def fit_polynomial_models(X_train, y_train, X_test, y_test, orders=[1, 2, 3]):
         polynomials[order] = model
 
     return polynomials, mse_values, predictions, chi2_values, bic_values
+
+
+def calculate_training_data(x_axis, y_axis, number_of_tests):
+    x_axis_scaled = x_axis - x_axis.min()
+
+    train_size = len(x_axis_scaled) - number_of_tests
+
+    X_train = x_axis_scaled[:train_size]
+    y_train = y_axis[:train_size]
+
+    X_test = x_axis_scaled[train_size:]
+    y_test = y_axis[train_size:]
+
+    return train_size, X_train, y_train, X_test, y_test
+
+
+def calculate_forecast(y_test, best_order, future_years, years, model, predictions):
+    future_scaled = future_years - years.min()
+    future_preds = model(future_scaled)
+
+    residuals = y_test - predictions[best_order]
+    std_error = np.std(residuals)
+
+    # 95% Confidence Intervals
+
+    upper_bound = future_preds + 1.96 * std_error
+    lower_bound = future_preds - 1.96 * std_error
+
+    return future_preds, lower_bound, upper_bound
